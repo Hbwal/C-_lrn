f7be385a6458d428b94bb5f5beaa2a5b4dd226c9
Hb_W <hugobarillasvi@hotmail.com>
2024-11-02 20:48:05 -0600

    Basic Variables in C++

--- Start.cpp
+++ Start.cpp
@@ -2,7 +2,21 @@
 
 int main(){
 
- 
-std::cout << "Hello World  ";
+// Variables
+
+    int a = 4;
+    int b = 2;
+    double z = 2.3; // double for decimals
+    char ch = 'A'; // just a character 
+    bool f = true; // like a switch true or false, just that
+
+    std::string name = "hb";
+
+    std::cout<<"Hello " << name << '\n';
+
+    int c = a + b;
+    std::cout << c;
+
+
 return 0;
 }
